251e4dc16cf118a5776989895569777ce6e49365
Yasser <aitelghariyasser@gmail.com>
2024-10-06 17:52:32 +0100

    handling validation of tags and getting tags from the browser to the sevrelets

---
+++ src/main/java/dev/yassiraitelghari/utils/TagErrors.java
@@ -0,0 +1,16 @@
+package dev.yassiraitelghari.utils;
+
+public class TagErrors {
+
+    private static String emptyTags = "Enter one Tag At least";
+
+
+
+    public static String getEmptyTags() {
+        return emptyTags;
+    }
+
+    public static  void setEmptyTags(String emptyTags) {
+        TagErrors.emptyTags = emptyTags;
+    }
+}
--- src/main/java/dev/yassiraitelghari/web/AddTagsServlet.java
+++ src/main/java/dev/yassiraitelghari/web/AddTagsServlet.java
@@ -1,11 +1,12 @@
 package dev.yassiraitelghari.web;
 
+import dev.yassiraitelghari.utils.TagErrors;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-
+import dev.yassiraitelghari.domain.User;
 import java.io.IOException;
 
 @WebServlet(name = "AddTagsServlet")
@@ -17,7 +18,24 @@ public class AddTagsServlet extends HttpServlet {
     }
 
     @Override
-    protected void doPost(HttpServletRequest request , HttpServletResponse response){
+    protected void doPost(HttpServletRequest request , HttpServletResponse response) throws IOException, ServletException {
+        if(request.getParameter("cancel")!=null){
+            User user = (User)request.getSession().getAttribute("user");
+            request.getSession().invalidate();
+            request.getSession().setAttribute("user",user);
+            response.sendRedirect("../profile");
+
+        }
+        if(request.getParameter("tags")!=null){
+            String tags = (String)request.getParameter("tags");
+            if(tags.isEmpty()){
+                request.setAttribute("tagsError",TagErrors.getEmptyTags());
+                request.getRequestDispatcher("/dashboard/addTags.jsp").forward(request,response);
+            }else{
+                String[] strings = tags.split(",");
+                request.getRequestDispatcher("../login.jsp").forward(request,response);
+            }
+        }
 
     }
 }
