640ef029c5a9e2a86de4bf6df55a781876222d20
Yasser <aitelghariyasser@gmail.com>
2024-10-08 13:15:30 +0100

    set count of delete to 0 whne the client user it

--- src/main/java/dev/yassiraitelghari/web/DeleteTaskServlet.java
+++ src/main/java/dev/yassiraitelghari/web/DeleteTaskServlet.java
@@ -3,10 +3,7 @@ package dev.yassiraitelghari.web;
 import dev.yassiraitelghari.domain.Task;
 import dev.yassiraitelghari.repositories.TaskRepository;
 import dev.yassiraitelghari.repositories.TaskRepositoryImp;
-import dev.yassiraitelghari.services.TagService;
-import dev.yassiraitelghari.services.TagServiceImp;
-import dev.yassiraitelghari.services.TaskService;
-import dev.yassiraitelghari.services.TaskServiceImp;
+import dev.yassiraitelghari.services.*;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -16,6 +13,7 @@ import java.io.IOException;
 public class DeleteTaskServlet extends HttpServlet {
     private TaskService taskService = new TaskServiceImp();
     private TagService tagService = new TagServiceImp();
+    private UserService userService = new UserServiceImp();
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
@@ -27,12 +25,14 @@ public class DeleteTaskServlet extends HttpServlet {
                 taskService.delete(parsedId);
             }
         } else {
-            if(task.getUser().getDeleteJeton()>0){
+            if (task.getUser().getDeleteJeton() > 0) {
                 if (tagService.deleteByTask(parsedId)) {
                     taskService.delete(parsedId);
                 }
-                //must make delete count 0
+                task.getUser().setDeleteJeton(0);
+                userService.update( task.getUser());
             }
+
         }
 
         response.sendRedirect("../dashboard");
